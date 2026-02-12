5e568e8dd2c0381062356166d7a4357d63617960
infoanil <info.anilchudasama@gmail.com>
2026-02-12 15:17:33 +0530

    fix: minor issue resolved

--- assets/blog-posts-list.js
+++ assets/blog-posts-list.js
@@ -1,10 +1,9 @@
-import PaginatedList from '@theme/paginated-list';
-
-/**
- * A custom element that renders a paginated blog posts list
- */
-export default class BlogPostsList extends PaginatedList {}
-
-if (!customElements.get('blog-posts-list')) {
-  customElements.define('blog-posts-list', BlogPostsList);
-}
+import PaginatedList from '@theme/paginated-list';
+/**
+ * A custom element that renders a paginated blog posts list
+ */
+export default class BlogPostsList extends PaginatedList {}
+;
+if (!customElements.get('blog-posts-list')) {
+  customElements.define('blog-posts-list', BlogPostsList);
+}
